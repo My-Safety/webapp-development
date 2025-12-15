dc7d4b413e760e2aaf81050fc338c4a253031923
adwindosakura <cadwa@indosakura.com>
2025-12-15 13:53:11 +0530

    baseurl change

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -2,8 +2,8 @@
 // Created By Suresh M, 11/11/2025
 // const String appDomain = 'http://192.168.0.120:4000/api/';
 
-const String appDomain = 'https://139.59.60.31:4000/api/';
-const String webSocket = 'https://192.168.0.120:4000';
+const String appDomain = 'https://139.59.60.31:4001/api/';
+const String webSocket = 'https://192.168.0.120:4001';
 const String googleMapKey = 'AIzaSyCI-T5kfFDaBGWiLZPAJXzbQa9v7miTYuE';
 const String encryptKey = 'ThisIsMy32LengthKeyForEncryption';
 const String encryptIV = '1234567890';
